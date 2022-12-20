bc06a41e98b92eb1a1cc8ae120b87525e002834a
Roni Yaniv <roniyaniv@gmail.com>
2022-12-20 10:39:39 +0200

    assert search results

--- features/DressSearch.feature
+++ features/DressSearch.feature
@@ -1,14 +1,14 @@
 Feature: Dress Search
 
-  As a user, I want to be able to search for dresses by type so I can purchase one
+  As a user, I want to be able to search for dresses on https://magento.softwaretestingboard.com by type so I can purchase one
 
   Background:
     Given the browser is at the "Home" page
 
   Scenario: Search for a type that does not exist
     When the user searches for "floral"
-    Then a no results error message is shown
-    And no products are listed
+    Then no products are listed
+    And a no results error message is shown
 
   Scenario: Search for a type that exists
     When the user searches for "striped"
--- steps/search.js
+++ steps/search.js
@@ -25,3 +25,18 @@ Given('the browser is at the {string} page', async (page) => {
 When('the user searches for {string}', async (s) => {
   await searchDressTypeBy(s)
 })
+
+Then(/(no )?products are listed/, async captured => { // using regex to pass capture groups to the function
+  if (captured === 'no '){ // space included for exact match to captured string from step definition
+    console.log('no products are listed')
+    await expect($('.message.notice > div')).toHaveTextContaining('Your search returned no results.')
+    
+  }else{
+    console.log('products ARE listed')
+    await expect($('.search.results')).toBeDisplayed()
+    await expect($('.message.notice > div')).not.toExist()
+    let resultsList = await $$('.products.wrapper.grid.products-grid > ol > li')
+    console.log('# of results: ' + resultsList.length)
+    expect(resultsList.length).toBeGreaterThan(0)
+  }
+})
